34897219456b57e0e07859524b4ae77f01d76f87
Sajjad Aslam <50263961+sajjad566@users.noreply.github.com>
2022-01-28 22:17:37 +0500

    Add files via upload

---
+++ resources/views/admin/viewUsers.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.adminNav')
+@section('userDeletion')
+<script>
+	function deleteUser(id) {
+		swal({
+		title: "Delete User and Their Data?",
+		text: "Are you sure you want to delete this user and their data (Invoices and Contracts)",
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+		})
+		.then((willDelete) => {
+		if (willDelete) {
+		swal("Deleting User and Its Data...", {
+		icon: "success",
+		});
+		window.location = "/deleteUser/"+id;
+		}
+		});
+	}
+</script>
+@endsection
+@section('content')
+<div class="container">
+	<div class="row">
+		@include('admin.adminControls.sideBar')
+		<div class="col-md-9">
+			<h1>All Users</h1>
+			@if (session('success'))
+				<div class="alert alert-success">
+		          {{ session('success') }}
+		        </div>
+		      @endif
+			@if ($errors->any())
+                <div class="alert alert-danger">
+                    @if($errors->has('policy_number'))
+                        <li>Please Write A Policy Number</li>
+                    @endif
+                </div>
+            @endif
+			<table id="all-users" class="display border p-2">
+				<thead>
+					<tr>
+						<th>User Name</th>
+						<th>Email</th>
+						<th>Profile</th>
+						<th>Remove User</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($users as $user)
+                    @if ($user->role_id == 1 || $user->role_id == 3)
+                    
+                    @else
+					<tr>
+						<td>{{ $user->name }}</td>
+						<td>{{ $user->email }}</td>
+						<td>
+						    @if ($user->role_id == 1)
+						    ---
+							@else
+    							<a href="/userprofile/{{ $user->id }}" target="_blank" class="btn btn-info">
+    							    <i class="fas fa-external-link-alt"></i>
+    							</a>
+							@endif
+						</td>
+						
+						<td>
+							@if ($user->role_id == 1)
+								---
+							@else
+								<button class="btn btn-danger" onclick="deleteUser({{ $user->id }})">Delete</button>
+							@endif
+						</td>
+					</tr>
+					@endif
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+@endsection
